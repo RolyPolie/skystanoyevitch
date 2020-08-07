e3728d1076b4e17b45d2a944328ba98ac400ff50
RolyPolie <skyfrom1987@gmail.com>
2020-08-07 12:53:26 -0700

    added JS modal function

---
+++ script.js
@@ -0,0 +1,10 @@
+
+    // Modal function
+const modal = document.querySelector(".modal");
+function toggleActive() {
+    modal.classList.toggle("is-active");
+    }
+
+    function modalClose() {
+        modal.classList.remove("is-active");
+}
