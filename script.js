c807817161c0bcdebc0cca63e7d8d044fd4721be
Sky <RolyPolie@users.noreply.github.com>
2020-08-13 16:31:16 -0700

    add JS hamburger functions

--- script.js
+++ script.js
@@ -8,3 +8,30 @@
 //     function modalClose() {
 //         modal.classList.remove("is-active");
 // }
+
+$(document).ready(function() {
+
+    // Check for click events on the navbar burger icon
+        $(".navbar-burger").click(function() {
+            
+            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+            $(".navbar-burger").toggleClass("is-active");
+            $(".navbar-menu").toggleClass("is-active");
+            // $('.navbar-dropdown').hide();
+
+
+
+            $('.has-dropdown').click(function(){
+                $('.navbar-dropdown').removeClass('is-hidden-mobile')
+                $('.navbar-dropdown').toggleClass('is-active')
+
+            })
+    
+        });
+
+        $('.has-dropdown').click(function(){
+            $('.has-dropdown').toggleClass('is-active');
+        })
+
+        
+    });
